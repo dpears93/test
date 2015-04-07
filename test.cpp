d8864526be8e80b0221fc6521e9566b7027b0582
davis <davis@localhost.localdomain>
2015-04-07 17:52:17 -0600

    second commit

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "This is a git test." << endl;
+	cout << "this is adding another line." << endl;
 	return 0;
 }
